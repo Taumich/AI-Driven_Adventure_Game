8bfd1761c7add017d147879fc7f2bb43c43a39e5
Taumich <michael.arenander@gmail.com>
2018-11-08 22:35:57 +0100

    Updated speech and action system for AI-foundation

--- src/maii/AI_Speech.java
+++ src/maii/AI_Speech.java
@@ -1,10 +1,98 @@
 package maii;
 
 public class AI_Speech {
-	int x = 1;
-
-	static int hello(int y) {
-		return y;
+	
+	static boolean devMode = Main.devMode;
+	
+	public static String[] AI(String answer, int[] coordinates)
+	{
+		answer = answer.toLowerCase();
+		answer = answer.replaceAll("[\\-\\+\\.\\^:,]","");
+		String[] aword = answer.split("\\s+");
+		
+		//Local variables
+		boolean equality = false,
+				log = true;
+		String[] respons = {"temp","x","x"};
+		//AI response library
+		
+		String[][] responses[] = 
+				{	
+					  {	{"esa"},
+						  	{"wrong","w"},
+						  	{"right","correct","r"},
+						  	{"retry","next","other","d"}
+					},{	{"hello","hi"},
+							{"Greetings!", "Hello good sir!"}
+					},{	{"heyo","yo","wazaa","wazzapp"},
+							{"Heyo!", "Bruh wazaaap!", "yo!"}
+					},{	{"yes","y","positive","yez","ye"},
+							{"That's great!","Amazing!","Splendid!","Excellent!","Superb!","Marvelous!"},
+							{"+"}
+					},{	{"why","how","how come","why so"},
+							{"That's why!", "No idea", "Why you asking me?"},
+							{"?"}
+					},{	{"beautiful","pretty","amazing","lovely","splendid"},
+							{"Yes you are! <3", "Aww thanks! <3", "You are so sweet! <3"}
+					},{	{"crazy","stupid","mad","insane","dumb"},
+							{"Oi you wanna fight?!", "Is that an insult?"}
+					}
+				},
+						actions =
+				{
+						{"Took a step north",			"up","north"},
+						{"Took a step south",			"down","south"},
+						{"you can't return from here",	"return","back"},
+						{"took a step east", 			"right","east"},
+						{"Took a step west",			"left","west"}
+				};
+		
+		Say("_____Testing action loop now_____", log && devMode);
+		
+		//Ask for Task
+		int i=0;
+		ActionLoops:
+		for (String[] act : actions) {
+			for (String syn : act) {
+				for (String word : aword) {
+					equality = new String(word).equals(syn);
+					if(equality) {
+						respons = WorldCode.World( (i-1)/2, coordinates);
+						break ActionLoops;
+					}
+				}
+			}
+			i++;
+		}
+		
+		Say("Action loop end\n_____Testing response loop now_____ ", log && devMode);
+		
+		//return result
+		if (!equality)
+		{	//Ask for Conversation
+			i=0;
+			for (String[][] speechtype : responses) {
+				for(String speech : speechtype[0]) {
+					for (String word : aword) {
+						equality = new String(word).equals(speech);
+						Say("Checking "+word+" equal to "+speech+" which is: "+equality+" "+i, log && devMode);
+						if(equality) {
+							Say("   Break", log && devMode);
+							int mod = (Main.turn) % (responses[i][1].length);
+							String res[] = {responses[i][1][mod],"x","x"};
+							return res;
+						}
+					}
+				}
+				i++;
+			}
+		}
+		
+		return respons;
 	}
-
+	
+	private static void Say(String print, boolean devMode) {
+		if(devMode)
+			System.out.println(print);
+	}
 }
--- src/maii/Main.java
+++ src/maii/Main.java
@@ -3,19 +3,18 @@ import java.util.*;
 
 class Main{
 	
-	static boolean devMode = true;
+	static boolean devMode = !true;
+	static int turn = 0;
 	
 	public static void main (String[] args)
 	{
 		//Lokala variabler
+		
 		boolean exit = false;
-		int turn = 0;
 		int[] coordinates = {2,2};
 		
-		
-		AI_Speech aispeech = new AI_Speech();
+		//AI_Speech aispeech = new AI_Speech();
 		//WorldCode worldcode = new WorldCode();
-		
 		
 		Say("Welcome to Michael AI-initiative! \nType something to interact");
 		Scanner s = new Scanner(System.in);
@@ -26,7 +25,7 @@ class Main{
 			Say("Coordinates: " + coordinates[0] +","+ coordinates[1], devMode);
 			
 			//answer
-			String[] answer = AI(s.nextLine(), coordinates);
+			String[] answer = AI_Speech.AI(s.nextLine(), coordinates);
 			
 			if(new String(answer[0]).equals("x"))
 			{
@@ -52,77 +51,11 @@ class Main{
 			Say(answer[0]);
 			
 			turn ++;
-			if (turn >= 20) {
+			if (turn >= 10) {
 				exit = true;
 				Say("Game Over!"); //to delete
 			}
 		}	
-	}
-	
-	
-	private static String[] AI(String answer, int[] coordinates) {
-		//Local variables
-		boolean equality = false,
-				log = !true;
-		String[] respons = {"temp","x","x"},
-		
-		//AI response library
-				responses = 
-		{	
-			"hey","wazapp",  "no plz","plz",  "bye","no way",  "why","that's why",  "beautiful","yes you are",
-			"ask me","are you crazy?",  "start","start what? Say start game to play",  "yes","amazing!",  "no","how sad",  "plz", "precisely!",
-			"are you smart?","very smart!",  "who are you?","I am your friend", "what's up?","not much bro", 
-			"hello","hey hey hey!",  "wazapp","wazapp boi",  "wazapp boi","hey that's my line!",  "start game","intializing..."
-		},
-				actions =
-		{
-			"up","took a step forward",   "down","took a step back",  "return","you can't return from here",
-			"right","you turn to the right",  "left","you turn to the left"
-		},
-				message = {"x","n","n"};
-		
-		Say("_____Testing action loop now_____ actionLenght: "+actions.length, log && devMode);
-		
-		//Ask for Task
-		for (int i=0; i < actions.length-1; i++) {
-			equality = new String(answer).equals(actions[i]);
-			
-			i++;
-			
-			if(equality) {
-				Say("  action["+i+"] = (" + actions[0] + ") should be: " + actions[i-1], log && devMode);
-				//respons = World( (i-1)/2 , coordinates );
-				respons = WorldCode.World( (i-1)/2, coordinates);
-				break;
-			}
-		}
-		
-		//return result
-		if (equality)
-			message = respons;
-		else
-		{
-			Say("_____Testing response loop now_____ responsLength = "+ responses.length, log && devMode);
-			//Ask for Conversation
-			for (int i=0; i < (responses.length-1); i++) {
-				Say("  Testing ["+i+"]: " + responses[i], log && devMode);
-				equality = new String(answer).equals(responses[i]);
-				
-				i++;
-				Say("  respons["+i+"] = (" + respons[0] + ") should become: " + responses[i], log && devMode);
-				respons[0] = responses[i];
-				Say("  respons["+i+"] = (" + respons[0] + ") should be: " + responses[i], log && devMode);
-				if(equality) {
-					Say("   Break", log && devMode);
-					String res[] = {respons[0],"x","x"};
-					return res;
-				}
-			}
-			//if (equality) {}
-		}
-		
-		//Say("message is now: " + message[0] +", "+ message[1] +", "+ message[2], log && devMode);
-		return message;
 	}
 
 	private static void Say(String print) {
